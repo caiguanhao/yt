3ab502cd37e9e35ff7584292160182e63dfbe82b
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-07-27 16:43:48 +0800

    move code to another file;

---
+++ data.js
@@ -0,0 +1,123 @@
+var Q          = require('q');
+var fs         = require('fs');
+var path       = require('path');
+var spawn      = require('child_process').spawn;
+
+var YTDir      = path.join(getUserHome(), '.config', 'yt');
+var CookieFile = path.join(YTDir, 'cookie.json');
+var CacheFile  = path.join(YTDir, 'cache.json');
+
+var INSTRUCTIONS = 'Follow these steps and then run this command again:\n' +
+'1. Open Google Chrome and right click the page and select Inspect Element.\n' +
+'2. Go to https://www.youtube.com/, log in if you don\'t have.\n' +
+'3. In Networks tab, click Documents and right click the first item in the \n' +
+'   list and click Copy as cURL.';
+
+module.exports = {
+  readCache: readCache,
+  checkConf: checkConf,
+  createCache: createCache
+};
+
+function getUserHome() {
+  return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+}
+
+function mkdirp(dirpath) {
+  dirpath.split(/[\/\\]/g).reduce(function(parts, part) {
+    parts += part + '/';
+    try {
+      fs.mkdirSync(path.resolve(parts));
+    } catch(e) {}
+    return parts;
+  }, '');
+}
+
+function isStr(what) {
+  return what && (typeof what === 'string');
+}
+
+function readConf() {
+  try {
+    var cookie = JSON.parse(fs.readFileSync(CookieFile).toString());
+    var sid = cookie.SID;
+    var hsid = cookie.HSID;
+    var ssid = cookie.SSID;
+    var login_info = cookie.LOGIN_INFO;
+    if (!isStr(sid) || !isStr(hsid) || !isStr(ssid) || !isStr(login_info)) {
+      throw undefined;
+    }
+    return 'SID=' + sid + '; HSID=' + hsid + '; SSID=' + ssid +
+      '; LOGIN_INFO=' + login_info;
+  } catch(e) {
+    return undefined;
+  }
+}
+
+function readCache() {
+  try {
+    return JSON.parse(fs.readFileSync(CacheFile).toString());
+  } catch(e) {
+    return undefined;
+  }
+}
+
+function createFile(filepath, data) {
+  if (!fs.existsSync(YTDir)) {
+    mkdirp(YTDir);
+  } else if (!fs.statSync(YTDir).isDirectory()) {
+    fs.unlinkSync(YTDir);
+    mkdirp(YTDir);
+  }
+  fs.writeFileSync(
+    filepath,
+    JSON.stringify(data, undefined, 2),
+    { mode: 0600 }
+  );
+}
+
+function createConf(data) {
+  createFile(CookieFile, data);
+}
+
+function createCache(data) {
+  createFile(CacheFile, data);
+}
+
+function checkConf() {
+  var conf = readConf();
+  if (conf) {
+    return Q(conf);
+  } else {
+    return getClipboard().then(function(content) {
+      var data = content.match(/\b(SID|HSID|SSID|LOGIN_INFO)=(.+?);/g);
+      if (!data || data.length !== 4) throw INSTRUCTIONS;
+      var conf = {};
+      for (var i = 0; i < data.length; i++) {
+        var d = data[i].split('=', 2);
+        conf[d[0]] = d[1].slice(0, -1);
+      }
+      createConf(conf);
+      return checkConf();
+    });
+  }
+}
+
+function getClipboard() {
+  var deferred = Q.defer();
+  var pbpaste = spawn('pbpaste');
+  var data = '';
+  pbpaste.stdout.on('data', function(chunk) {
+    data += chunk;
+  });
+  pbpaste.stdout.on('end', function() {
+    deferred.resolve(data);
+  });
+  pbpaste.on('exit', function(code) {
+    deferred.reject('Exit with code: '+ code);
+  });
+  pbpaste.on('error', function(e) {
+    deferred.reject(e.message);
+  });
+  return deferred.promise;
+}
--- yt.js
+++ yt.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 var Q          = require('q');
-var fs         = require('fs');
-var path       = require('path');
 var events     = require('events');
 var https      = require('https');
+var libdata    = require('./data');
 var entities   = require('entities');
 var termMenu   = require('terminal-menu-2');
 var htmlparser = require('htmlparser2');
@@ -12,9 +11,6 @@ var spawn      = require('child_process').spawn;
 
 var COLUMNS    = process.stdout.columns || 80;
 var ROWS       = process.stdout.rows || 24;
-var YTDir      = path.join(getUserHome(), '.config', 'yt');
-var CookieFile = path.join(YTDir, 'cookie.json');
-var CacheFile  = path.join(YTDir, 'cache.json');
 var COOKIE, MENU;
 
 var colMax = COLUMNS - 4;
@@ -22,21 +18,15 @@ var rowMax = ROWS - 2;
 
 var ITEMSPERPAGE = 16;
 
-var INSTRUCTIONS = 'Follow these steps and then run this command again:\n' +
-'1. Open Google Chrome and right click the page and select Inspect Element.\n' +
-'2. Go to https://www.youtube.com/, log in if you don\'t have.\n' +
-'3. In Networks tab, click Documents and right click the first item in the \n' +
-'   list and click Copy as cURL.';
-
 var RUNNING = [], ITEMS = [];
 
 Q().
 then(function() {
-  ITEMS = readCache();
+  ITEMS = libdata.readCache();
   makeMenu();
 }).
 then(function() {
-  return checkConf();
+  return libdata.checkConf();
 }).
 then(function(cookie) {
   COOKIE = cookie;
@@ -59,7 +49,7 @@ then(function(res) {
 }).
 then(function(data) {
   if (!arrayEquals(ITEMS, data)) {
-    createCache(data);
+    libdata.createCache(data);
     ITEMS = data;
     makeMenu();
   }
@@ -100,111 +90,8 @@ ytEvents.on('end', function(url) {
 
 
 
-function getUserHome() {
-  return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-}
-
-function mkdirp(dirpath) {
-  dirpath.split(/[\/\\]/g).reduce(function(parts, part) {
-    parts += part + '/';
-    try {
-      fs.mkdirSync(path.resolve(parts));
-    } catch(e) {}
-    return parts;
-  }, '');
-}
-
 function arrayEquals(a, b) {
   return JSON.stringify(a) === JSON.stringify(b);
-}
-
-function isStr(what) {
-  return what && (typeof what === 'string');
-}
-
-function readConf() {
-  try {
-    var cookie = JSON.parse(fs.readFileSync(CookieFile).toString());
-    var sid = cookie.SID;
-    var hsid = cookie.HSID;
-    var ssid = cookie.SSID;
-    var login_info = cookie.LOGIN_INFO;
-    if (!isStr(sid) || !isStr(hsid) || !isStr(ssid) || !isStr(login_info)) {
-      throw undefined;
-    }
-    return 'SID=' + sid + '; HSID=' + hsid + '; SSID=' + ssid +
-      '; LOGIN_INFO=' + login_info;
-  } catch(e) {
-    return undefined;
-  }
-}
-
-function readCache() {
-  try {
-    return JSON.parse(fs.readFileSync(CacheFile).toString());
-  } catch(e) {
-    return undefined;
-  }
-}
-
-function createFile(filepath, data) {
-  if (!fs.existsSync(YTDir)) {
-    mkdirp(YTDir);
-  } else if (!fs.statSync(YTDir).isDirectory()) {
-    fs.unlinkSync(YTDir);
-    mkdirp(YTDir);
-  }
-  fs.writeFileSync(
-    filepath,
-    JSON.stringify(data, undefined, 2),
-    { mode: 0600 }
-  );
-}
-
-function createConf(data) {
-  createFile(CookieFile, data);
-}
-
-function createCache(data) {
-  createFile(CacheFile, data);
-}
-
-function checkConf() {
-  var conf = readConf();
-  if (conf) {
-    return Q(conf);
-  } else {
-    return getClipboard().then(function(content) {
-      var data = content.match(/\b(SID|HSID|SSID|LOGIN_INFO)=(.+?);/g);
-      if (!data || data.length !== 4) throw INSTRUCTIONS;
-      var conf = {};
-      for (var i = 0; i < data.length; i++) {
-        var d = data[i].split('=', 2);
-        conf[d[0]] = d[1].slice(0, -1);
-      }
-      createConf(conf);
-      return checkConf();
-    });
-  }
-}
-
-function getClipboard() {
-  var deferred = Q.defer();
-  var pbpaste = spawn('pbpaste');
-  var data = '';
-  pbpaste.stdout.on('data', function(chunk) {
-    data += chunk;
-  });
-  pbpaste.stdout.on('end', function() {
-    deferred.resolve(data);
-  });
-  pbpaste.on('exit', function(code) {
-    deferred.reject('Exit with code: '+ code);
-  });
-  pbpaste.on('error', function(e) {
-    deferred.reject(e.message);
-  });
-  return deferred.promise;
 }
 
 function request(page) {
@@ -278,6 +165,9 @@ function pad(n) {
 }
 
 function makeMenu() {
+  if (Object.prototype.toString.call(ITEMS) !== '[object Array]') return;
+  if (ITEMS.length === 0) return;
+
   var selected = -1;
   if (MENU) {
     selected = MENU.selected;
